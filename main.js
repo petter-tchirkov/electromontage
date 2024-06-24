4da70dd5b2b64df385b15fa27ec620ce43f6fade
Chirkov Petro <tchirkov.petter@gmail.com>
2024-06-24 08:47:56 +0300

    init

--- main.js
+++ main.js
@@ -17,3 +17,19 @@ new Swiper('.swiper', {
         prevEl: '.swiper-prev',
     },
 });
+
+new Swiper('.video-swiper', {
+    centeredSlides: true,
+    slidesPerView: 1,
+    loop: true,
+    spaceBetween: 20,
+    pagination: {
+        el: '.video-pag',
+        clickable: true,
+        type: 'fraction'
+    },
+    navigation: {
+        nextEl: '.video-next',
+        prevEl: '.video-prev',
+    },
+});
